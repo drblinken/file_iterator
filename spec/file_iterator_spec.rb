18a1773d6e1842fe41b98db58c47e563256a2719
B Kleinen <kleinen@infrastructure.de>
2014-11-09 22:59:22 +0100

    added testcases for three usages

--- spec/file_iterator_spec.rb
+++ spec/file_iterator_spec.rb
@@ -1,14 +1,26 @@
 require 'file_iterator'
 describe FileIterator::FileIterator do
-	describe "constructor" do
+	describe "collections" do
 		it "just initializes if no block is given" do
 			fi = FileIterator::FileIterator.new('numbers.txt')
 		end
 		it "iterates if block is given" do
 			result = []
-			fi = FileIterator::FileIterator.new('spec/numbers.txt') do | line |
+			FileIterator::FileIterator.new('spec/numbers.txt') do | line |
 				result << line.to_i
 			end
+			expect(result).to eq [1,2,3,4]
+		end
+		it "iterates" do
+			result = []
+			fi = FileIterator::FileIterator.new('spec/numbers.txt') 
+			fi.iterate do | line |
+				result << line.to_i
+			end
+			expect(result).to eq [1,2,3,4]
+		end
+		it "converts to array" do
+			result = FileIterator::FileIterator.new('spec/numbers.txt').to_a.map(&:to_i)
 			expect(result).to eq [1,2,3,4]
 		end
 	end
